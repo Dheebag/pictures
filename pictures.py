d704439d112476b93d057c80cd1b1805a0336064
dheebag <dheebag@yahoo.com>
2018-04-01 16:53:12 -0700

    working version with list of dicts as the data structure

--- pictures.py
+++ pictures.py
@@ -14,23 +14,31 @@ def display_files():
         my_base_url = request.base_url
         print "My base url %s" % my_base_url
 	my_path= os.path.join(request.args.get('path',''))
-	print my_path
-
+	my_list_of_dict=[]
 
 	if dir == "":
 		dir = "/"
-	print "DIR is %s" % dir
-	print os.listdir(dir)
-	if os.path.isdir(dir): 
+	if os.path.isdir(dir):
 		mydirs = next(os.walk(dir))[1]
+		for d in mydirs:
+			my_list_of_dict.append({"Directory":d}) 
+		
 		myfiles = next(os.walk(dir))[2]
+		for f in myfiles:
+			my_list_of_dict.append({"File":f}) 
 		#myfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) ]
-		print mydirs, myfiles
-		if mydirs == "":
+		print my_list_of_dict
+		for i in my_list_of_dict:
+			if i.get('Directory') != None:  
+				print i.get('Directory') 
+			else:
+				print i.get('File')
+		
+		if my_list_of_dict == "":
 			return "No directories !"
 		else:
 
-			return render_template('index.html', dir=os.listdir(dir),my_base_url=my_base_url,my_path=my_path,dirs = mydirs, files=myfiles)
+			return render_template('index.html', dir=os.listdir(dir),my_base_url=my_base_url,my_path=my_path,my_dict = my_list_of_dict, files=myfiles)
 			#return jsonify(Directories=mydirs, Files=myfiles) 
 	else:
 		return "%s Dir doesn't exist" % dir
